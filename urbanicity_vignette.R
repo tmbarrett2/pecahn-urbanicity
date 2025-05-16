5c7043a4065ee402ff2d31fa86825d2fc1b776c2
Tyler Barrett <85252584+tmbarrett2@users.noreply.github.com>
2025-05-16 11:00:47 -0600

    update vignette

--- urbanicity_vignette.R
+++ urbanicity_vignette.R
@@ -8,20 +8,36 @@
   #fp = "your_file_path"
 
 # Load Test Data
-  # The test_data_boundaries example provides northeast, southeast, southwest, and northwest coordinates for seven communities
+  # The test_data_boundaries data table provides northeast, southeast, southwest, and northwest coordinates for seven communities.
     test_data_boundaries <- read.csv(paste0(fp, "/test_data_boundaries.csv"))
     
-  # The test_data_5km example provides a single coordinate for 67 communities.
+  # The test_data_5km data table provides a single coordinate for 38 communities.
     test_data_5km <- read.csv(paste0(fp, "/test_data_5km.csv"))
   
 # Load Source Functions
   source(paste0(fp, "/urbanicity_source_functions.r"))
 
 # Create Bounding Box for Each Test Dataset.
-# The bounding box defines the area that we are pulling data for.
-  bbox_boundaries <- create_bounding_boxes(test_data_boundaries)
-  bbox_5km <- create_bounding_boxes(test_data_5km)
+  # The bounding box defines the area that we are pulling data for.
+  # For the test_data_boundaries example, we pre-specify the full boundary area with exact coordinates.
+  # For the test_data_5km example, we define the boundary as 5km with in a single point.
+  # The function defaults to 5km, but you can specify any distance by changing the distance_km value.
+    
+    bbox_boundaries <- create_bounding_boxes(test_data_boundaries)
+    bbox_5km <- create_bounding_boxes(test_data_5km, distance_km = 5)
   
-# Compute urbanicity measures...
-  test_results_boundaries <- compute_urbanicity_iterative(bbox_boundaries, metrics = c("all"))
-  test_results_5km <- compute_urbanicity_iterative(bbox_5km, metrics = c("all"))
+# Compute Urbanicity Measures
+  # This function takes the bounding box (bbox) we created above as the main argument.
+  # You also need to specify which metrics to calculate. The default is to calculate all metrics,
+  # but you can also change replace "all" below with any combination of the following to calculate only those metrics:
+    # roads (percent of paved roads and ratio of paved-to-unpaved roads)
+    # shops (number of formal shops / markets)
+    # healthcare (presence of healthcare facilities [yes/no])
+    # transport (number of public transit stops)
+    # financial (number of financial facilities [e.g., atms, banks])
+    # schools (presence of schools [yes/no])
+    # cell_towers (number of cell towers)
+    # buildings (building density [i.e., percent of area covered by buildings])
+    
+      test_results_boundaries <- compute_urbanicity_iterative(bbox_boundaries, metrics = c("all"))
+      test_results_5km <- compute_urbanicity_iterative(bbox_5km, metrics = c("all"))
